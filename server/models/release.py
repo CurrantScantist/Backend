5cd4f921435b2b081baafe76e6adfc748d7e1413
dslguo <sguo0011@student.monash.edu>
2021-08-10 23:02:09 +1000

    test: testing for release metadata
    Also changed schema file

--- server/models/release.py
+++ server/models/release.py
@@ -12,24 +12,81 @@ class releaseSchema(BaseModel):
 
     class Config:
         schema_extra = {
-            "example": {
-                "id": "60fc0777f113c31336c7e596",
-                "name": "vue",
-                "owner": "vuejs",
-                "tag_name": "Cybernetically enhanced web apps",
-                "LOC": {
-                    "TypeScript": {
-                        "nfiles": 60,
-                        "blank": 3112
-                        },
-                    "JavaScript":{
-                        "nfiles": 212,
-                        "blank": 2214
-                    }
-                },
-                "committed_date":"2032-04-23T10:20:30.400+02:30"
-            }
-        }
+        "id": "610912e6799bad69e5fc2640",
+        "name": "vue",
+        "owner": "vuejs",
+        "tag_name": "v2.6.14",
+        "LOC": {
+          "JavaScript": {
+            "nFiles": 431,
+            "blank": 15859,
+            "comment": 17485,
+            "code": 137694
+          },
+          "TypeScript": {
+            "nFiles": 19,
+            "blank": 230,
+            "comment": 78,
+            "code": 1595
+          },
+          "HTML": {
+            "nFiles": 18,
+            "blank": 83,
+            "comment": 39,
+            "code": 1280
+          },
+          "Markdown": {
+            "nFiles": 13,
+            "blank": 305,
+            "comment": 0,
+            "code": 1206
+          },
+          "CSS": {
+            "nFiles": 9,
+            "blank": 130,
+            "comment": 8,
+            "code": 717
+          },
+          "Vuejs Component": {
+            "nFiles": 27,
+            "blank": 50,
+            "comment": 12,
+            "code": 644
+          },
+          "JSON": {
+            "nFiles": 12,
+            "blank": 0,
+            "comment": 0,
+            "code": 339
+          },
+          "YAML": {
+            "nFiles": 3,
+            "blank": 9,
+            "comment": 2,
+            "code": 139
+          },
+          "Bourne Shell": {
+            "nFiles": 2,
+            "blank": 16,
+            "comment": 17,
+            "code": 85
+          },
+          "Bourne Again Shell": {
+            "nFiles": 2,
+            "blank": 4,
+            "comment": 1,
+            "code": 16
+          },
+          "SUM": {
+            "blank": 16686,
+            "comment": 17642,
+            "code": 143715,
+            "nFiles": 536
+          }
+        },
+        "committed_date": "2021-06-07T09:55:28"
+        
+    }
 
 
 class UpdateReleaseModel(BaseModel):
@@ -42,24 +99,81 @@ class UpdateReleaseModel(BaseModel):
     
     class Config:
         schema_extra = {
-            "example": {
-                "id": "60fc0777f113c31336c7e596",
-                "name": "vue",
-                "owner": "vuejs",
-                "tag_name": "Cybernetically enhanced web apps",
-                "LOC": {
-                    "TypeScript": {
-                        "nfiles": 60,
-                        "blank": 3112
-                        },
-                    "JavaScript":{
-                        "nfiles": 212,
-                        "blank": 2214
-                    }
-                },
-                "committed_date":"2032-04-23T10:20:30.400+02:30"
-            }
-        }
+        "id": "610912e6799bad69e5fc2640",
+        "name": "vue",
+        "owner": "vuejs",
+        "tag_name": "v2.6.14",
+        "LOC": {
+          "JavaScript": {
+            "nFiles": 431,
+            "blank": 15859,
+            "comment": 17485,
+            "code": 137694
+          },
+          "TypeScript": {
+            "nFiles": 19,
+            "blank": 230,
+            "comment": 78,
+            "code": 1595
+          },
+          "HTML": {
+            "nFiles": 18,
+            "blank": 83,
+            "comment": 39,
+            "code": 1280
+          },
+          "Markdown": {
+            "nFiles": 13,
+            "blank": 305,
+            "comment": 0,
+            "code": 1206
+          },
+          "CSS": {
+            "nFiles": 9,
+            "blank": 130,
+            "comment": 8,
+            "code": 717
+          },
+          "Vuejs Component": {
+            "nFiles": 27,
+            "blank": 50,
+            "comment": 12,
+            "code": 644
+          },
+          "JSON": {
+            "nFiles": 12,
+            "blank": 0,
+            "comment": 0,
+            "code": 339
+          },
+          "YAML": {
+            "nFiles": 3,
+            "blank": 9,
+            "comment": 2,
+            "code": 139
+          },
+          "Bourne Shell": {
+            "nFiles": 2,
+            "blank": 16,
+            "comment": 17,
+            "code": 85
+          },
+          "Bourne Again Shell": {
+            "nFiles": 2,
+            "blank": 4,
+            "comment": 1,
+            "code": 16
+          },
+          "SUM": {
+            "blank": 16686,
+            "comment": 17642,
+            "code": 143715,
+            "nFiles": 536
+          }
+        },
+        "committed_date": "2021-06-07T09:55:28"
+        
+    }
 
 
 def ResponseModel(data, message):
