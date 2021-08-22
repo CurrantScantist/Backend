7c614b75c0bc53474a987b9bc01cd45a208cdbf4
shourya <sraj0008@student.monash.edu>
2021-08-23 01:39:01 +1000

    updated the cors

--- server/app.py
+++ server/app.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from server.routes.techstack import router as TechstackRouter # Wiring up the techstack route in app/server/app.py
 from server.routes.release import router as ReleaseRouter # Wiring up the techstack route in app/server/app.py
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["http://119.181.73:8083", "https://119.181.73:8083"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(TechstackRouter, tags=["Techstack"], prefix="/techstack")
 app.include_router(ReleaseRouter, tags=["Release"], prefix="/release")
@@ -11,4 +22,3 @@ app.include_router(ReleaseRouter, tags=["Release"], prefix="/release")
 @app.get("/", tags=["Root"])
 async def read_root():
     return {"message": "Welcome to this fantastic app!"}
-
